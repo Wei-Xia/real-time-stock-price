0db16fce25742dd2939a1491e2fec133f672f75c
Wei Xia <xw900812@gmail.com>
2017-03-06 10:15:32 -0500

    fdjsfjksd

--- Real Time Stock Price/ViewController.swift
+++ Real Time Stock Price/ViewController.swift
@@ -25,12 +25,22 @@ class ViewController: UIViewController, UITextFieldDelegate, URLSessionDelegate,
         
         return task
     }
-
+    
+    func convertToDictionary(text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { (timer) in
+        Timer.scheduledTimer(withTimeInterval: 10, repeats: true) { (timer) in
             self.sendRequest(url: "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=IBM", completionHandler:{data, response, error in
                 
                 guard error == nil && data != nil else {
@@ -38,12 +48,22 @@ class ViewController: UIViewController, UITextFieldDelegate, URLSessionDelegate,
                     return
                 }
                 
-                let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                let responseNSString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
                 
-                print("responseString = \(responseString)")
+                let responseString = responseNSString as! String
                 
+                let dict = self.convertToDictionary(text: responseString)
+                
+                print(dict)
+                
+                let json = JSON(dict)
+
+                let price = json["LastPrice"].stringValue
+                
+                print(price)
+
                 DispatchQueue.main.async {
-                    self.PriceBox.text = responseString as String?
+                    self.PriceBox.text = price as String?
                 }
             })
             print("fire")
